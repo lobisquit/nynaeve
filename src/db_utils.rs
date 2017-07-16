fb1cd909196d63c7d000d0fa77885b458232bc65
Enrico Lovisotto <enricolovisotto@gmail.com>
2017-07-16 20:36:12 +0200

    First working database with almost all is needed

---
+++ src/db_utils.rs
@@ -0,0 +1,194 @@
+use model::*;
+use model::Weekday::*;
+
+use postgres::{Connection, TlsMode};
+
+use std::cmp::Ordering;
+use std::fmt;
+
+extern crate chrono;
+use chrono::NaiveTime;
+use chrono::Timelike;
+
+pub struct Database<'a> {
+    conn: &'a Connection,
+}
+
+impl<'a> Database<'a> {
+    pub fn new(conn: &'a Connection) -> Database<'a> {
+        Database { conn: conn }
+    }
+
+    pub fn init(&self, reset: bool) {
+        if reset {
+            // one execute can have only a single query, loop needed
+            let commands = [
+                "DROP TABLE IF EXISTS places CASCADE;",
+                "DROP TABLE IF EXISTS weekdays CASCADE;",
+                "DROP TABLE IF EXISTS alarms CASCADE;",
+                "DROP TABLE IF EXISTS weekdays_alarms CASCADE;",
+            ];
+
+            for command in &commands {
+                self.conn
+                    .execute(command, &[])
+                    .ok()
+                    .expect(format!("Query '{}' failed", command).as_ref());
+            }
+        }
+        let commands = [
+            "CREATE TABLE IF NOT EXISTS places (
+                name VARCHAR NOT NULL,
+                PRIMARY KEY (name)
+            );",
+            "CREATE TABLE IF NOT EXISTS weekdays (
+                num INTEGER NOT NULL,
+                PRIMARY KEY (num)
+            );",
+            "CREATE TABLE IF NOT EXISTS alarms (
+                id SERIAL NOT NULL,
+                time TIME NOT NULL,
+                place_name VARCHAR NOT NULL,
+                PRIMARY KEY (id),
+                UNIQUE (time, place_name),
+                FOREIGN KEY (place_name) REFERENCES places(name) ON DELETE CASCADE
+            );",
+            "CREATE TABLE IF NOT EXISTS weekdays_alarms (
+                weekday_num INTEGER NOT NULL,
+                alarm_id INTEGER NOT NULL,
+                FOREIGN KEY (weekday_num) REFERENCES weekdays(num),
+                FOREIGN KEY (alarm_id) REFERENCES alarms(id) ON DELETE CASCADE,
+                UNIQUE (weekday_num, alarm_id)
+            );",
+        ];
+        // create tables
+        for command in &commands {
+            self.conn
+                .execute(command, &[])
+                .ok()
+                .expect(format!("Query '{}' failed", command).as_ref());
+        }
+
+        for day in 0..7 {
+            self.conn
+                .execute("INSERT INTO weekdays (num) VALUES ($1);", &[&day])
+                .unwrap();
+        }
+    }
+
+    pub fn add_place(&self, place: &Place) {
+        let name: &str = place.name().as_ref();
+        self.conn
+            .execute("INSERT INTO places (name) VALUES ($1);", &[&name])
+            .unwrap();
+    }
+
+    pub fn get_places(&self) -> Vec<Place> {
+        let mut places = vec![];
+        for row in &self.conn.query("SELECT name FROM places;", &[]).unwrap() {
+            let name: String = row.get("name");
+            places.push(Place::new(name));
+        }
+        places
+    }
+
+    pub fn remove_place(&self, place: Place) -> Result<(), String> {
+        if !self.get_places().contains(&place) {
+            Err("No such place in database".to_owned())
+        } else {
+            &self.conn
+                .query("DELETE FROM places WHERE name = $1", &[&place.name()])
+                .unwrap();
+            Ok(())
+        }
+    }
+
+    pub fn add_alarm(&self, alarm: &Alarm) {
+        // time is converted to NaiveTime to put it easily into queries
+        let time: NaiveTime = alarm.time().into();
+        let place = alarm.place();
+        let weekdays = alarm.weekdays();
+
+        // add place if not in db
+        if !self.get_places().contains(&place) {
+            self.add_place(place);
+        }
+
+        // add alarm to its table
+        self.conn
+            .execute(
+                "INSERT INTO alarms (time, place_name) values ($1, $2);",
+                &[&time, &place.name()],
+            )
+            .unwrap();
+
+        // add weekday to weekdays_alarms joint table
+        for day in weekdays {
+            let num: i32 = day.into();
+            self.conn
+                .execute(
+                    "INSERT INTO weekdays_alarms
+                    (SELECT $1, id FROM alarms
+                        WHERE alarms.time = $2
+                        AND alarms.place_name = $3);",
+                    &[&num, &time, &place.name()],
+                )
+                .unwrap();
+        }
+    }
+
+    pub fn get_alarms(&self) -> Vec<Alarm> {
+        let mut alarms = vec![];
+
+        let alarms_query = self.conn
+            .query(
+                "SELECT id, place_name, time
+                FROM alarms, places
+                WHERE alarms.place_name = places.name;",
+                &[],
+            )
+            .unwrap();
+        for row in &alarms_query {
+            let id: i32 = row.get("id");
+
+            let place_name: String = row.get("place_name");
+            let naive_time: NaiveTime = row.get("time");
+            let time: Time = naive_time.into();
+
+            // retrieve weekdays
+            let weekdays_query = self.conn
+                .query(
+                    "SELECT weekday_num
+                    FROM weekdays_alarms
+                    WHERE weekdays_alarms.alarm_id = $1;",
+                    &[&id],
+                )
+                .unwrap();
+
+            let mut weekdays = vec![];
+            for combination_row in &weekdays_query {
+                let weekday = Weekday::from_int(combination_row.get("weekday_num")).unwrap();
+                weekdays.push(weekday);
+            }
+            alarms.push(Alarm::new(Place::new(place_name), weekdays, time));
+        }
+        alarms
+    }
+
+    pub fn remove_alarm(&self, alarm: Alarm) -> Result<(), String> {
+        if !self.get_alarms().contains(&alarm) {
+            Err("No such alarm in database".to_owned())
+        } else {
+            let time: NaiveTime = alarm.time().into();
+            &self.conn
+                .query(
+                    "DELETE FROM alarms WHERE
+                    alarms.place_name = $1 AND
+                    alarms.time = $2",
+                    &[&alarm.place().name(), &time],
+                )
+                .unwrap();
+            Ok(())
+        }
+    }
+}
--- src/model.rs
+++ src/model.rs
@@ -1,5 +1,9 @@
-use std::fmt;
+extern crate chrono;
+
+use self::chrono::NaiveTime;
+use self::chrono::Timelike;
 use std::cmp::Ordering;
+use std::fmt;
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Weekday {
@@ -9,25 +13,25 @@ pub enum Weekday {
     Thursday,
     Friday,
     Saturday,
-    Sunday
+    Sunday,
 }
 
-impl<'a> Into<usize> for &'a Weekday {
-    fn into(self) -> usize {
+impl<'a> Into<i32> for &'a Weekday {
+    fn into(self) -> i32 {
         match *self {
-            Weekday::Monday    => 0,
-            Weekday::Tuesday   => 1,
+            Weekday::Monday => 0,
+            Weekday::Tuesday => 1,
             Weekday::Wednesday => 2,
-            Weekday::Thursday  => 3,
-            Weekday::Friday    => 4,
-            Weekday::Saturday  => 5,
-            Weekday::Sunday    => 6
+            Weekday::Thursday => 3,
+            Weekday::Friday => 4,
+            Weekday::Saturday => 5,
+            Weekday::Sunday => 6,
         }
     }
 }
 
 impl Weekday {
-    pub fn from_int(value: usize) -> Result<Weekday, String> {
+    pub fn from_int(value: i32) -> Result<Weekday, String> {
         match value {
             0 => Ok(Weekday::Monday),
             1 => Ok(Weekday::Tuesday),
@@ -36,7 +40,7 @@ impl Weekday {
             4 => Ok(Weekday::Friday),
             5 => Ok(Weekday::Saturday),
             6 => Ok(Weekday::Sunday),
-            _ => Err(format!("{} is not in 0..7: invalid weekday number", value))
+            _ => Err(format!("{} is not in 0..7: invalid weekday number", value)),
         }
     }
 }
@@ -53,30 +57,32 @@ impl Place {
 
     pub fn new<S: Into<String>>(name: S) -> Place {
         let place_name: String = name.into();
-        Place{ name: place_name }
+        Place { name: place_name }
     }
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Time {
-    hour: usize,
-    minute: usize,
+    hour: i32,
+    minute: i32,
 }
 
 impl Time {
-    pub fn hour(&self) -> usize {
+    pub fn hour(&self) -> i32 {
         self.hour
     }
 
-    pub fn minute(&self) -> usize {
+    pub fn minute(&self) -> i32 {
         self.minute
     }
 
-    pub fn new(hour: usize, minute: usize) -> Result<Time, String> {
+    pub fn new(hour: i32, minute: i32) -> Result<Time, String> {
         if hour <= 24 && minute <= 60 {
-            Ok(Time {hour: hour, minute: minute})
-        }
-        else {
+            Ok(Time {
+                hour: hour,
+                minute: minute,
+            })
+        } else {
             Err(format!("{}:{} is and invalid Time", hour, minute))
         }
     }
@@ -86,37 +92,49 @@ impl PartialOrd for Time {
     fn partial_cmp(&self, other: &Time) -> Option<Ordering> {
         if self.hour == other.hour {
             Some(self.minute.cmp(&other.minute))
-        }
-        else {
+        } else {
             Some(self.hour.cmp(&other.hour))
         }
     }
 }
 
-impl fmt::Display for Time {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}:{}", self.hour, self.minute)
+// conversion from / into NaiveTime gives us ToSql trait
+// I cannot find an actual implementation of such trait
+
+impl<'a> Into<NaiveTime> for &'a Time {
+    fn into(self) -> NaiveTime {
+        NaiveTime::from_hms(self.hour as u32, self.minute as u32, 0)
+    }
+}
+
+impl From<NaiveTime> for Time {
+    fn from(t: NaiveTime) -> Self {
+        Time::new(t.hour() as i32, t.minute() as i32).unwrap()
     }
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Alarm {
     place: Place,
-    days: Vec<Weekday>,
+    weekdays: Vec<Weekday>,
     time: Time,
 }
 
 impl Alarm {
     pub fn new(place: Place, days: Vec<Weekday>, time: Time) -> Alarm {
-        Alarm { place: place, days: days, time: time }
+        Alarm {
+            place: place,
+            weekdays: days,
+            time: time,
+        }
     }
 
     pub fn place(&self) -> &Place {
         &self.place
     }
 
-    pub fn days(&self) -> &Vec<Weekday> {
-        &self.days
+    pub fn weekdays(&self) -> &Vec<Weekday> {
+        &self.weekdays
     }
 
     pub fn time(&self) -> &Time {
@@ -127,8 +145,8 @@ impl Alarm {
         // check place and time are the same
         if other.place.eq(&self.place) && self.time.eq(&other.time) {
             // check if self.days is a subset of other.days
-            for self_day in &self.days {
-                if ! other.days.contains(self_day) {
+            for self_day in &self.weekdays {
+                if !other.weekdays.contains(self_day) {
                     return false;
                 }
             }
